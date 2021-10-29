710786548324bd9b474c88a1fc627169718eda27
venkatrajula77@gmail.com <root@venkat.us-central1-c.c.global-calling-325109.internal>
2021-10-29 10:20:24 +0000

    Modified new src code

--- src/main/java/com/tcs/App.java
+++ src/main/java/com/tcs/App.java
@@ -4,6 +4,7 @@ package com.tcs;
  * Hello world!
  * Welcome to Devops with GCP!!!
  * DevOps is a culture between Development and Operations teams!!!
+ * Traniners are @@Kiran@@ @@Muneesh@@ @@Sathya@@ !!!!!!
  *
  */
 public class App 
@@ -14,7 +15,10 @@ public class App
     {
         System.out.println( "Welcome to Devops with GCP!!!" );
     {
-   System.out.println( "DevOps is a culture between Development and Operations teams!!!");
+        System.out.println( "DevOps is a culture between Development and Operations teams!!!");
+    {
+        System.out.println( "Traniners are @@Kiran@@ @@Muneesh@@ @@Sathya@@ !!!!!!");
+    }
     }
     }
     }
