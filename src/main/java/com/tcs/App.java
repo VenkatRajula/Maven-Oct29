5c015b841f907a185684de1d21aa5f68b8a00f83
VenkatRajula <root@vm1.us-central1-a.c.global-calling-325109.internal>
2021-10-29 06:36:56 +0000

    New project code

---
+++ src/main/java/com/tcs/App.java
@@ -0,0 +1,17 @@
+package com.tcs;
+
+/**
+ * Hello world!
+ * Welcome to Devops with GCP!!!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    {
+        System.out.println( "Welcome to Devops with GCP!!!" );
+    }
+    }
+}
