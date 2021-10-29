5b91c178b05246dc9f26467a8901fecab701bbef
root <root@venkat.us-central1-c.c.global-calling-325109.internal>
2021-10-29 10:08:50 +0000

    Added new branch and modified pom.xml and code

--- src/main/java/com/tcs/App.java
+++ src/main/java/com/tcs/App.java
@@ -3,6 +3,7 @@ package com.tcs;
 /**
  * Hello world!
  * Welcome to Devops with GCP!!!
+ * DevOps is a culture between Development and Operations teams!!!
  *
  */
 public class App 
@@ -12,6 +13,8 @@ public class App
         System.out.println( "Hello World!" );
     {
         System.out.println( "Welcome to Devops with GCP!!!" );
+    {   System.out.println( "DevOps is a culture between Development and Operations teams!!!");
+    }
     }
     }
 }
